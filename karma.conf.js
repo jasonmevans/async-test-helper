c5faffa8bd4cd56003c1a27d9762fbc4deb699fc
Jason Evans <nosaj311@gmail.com>
2017-08-26 19:40:34 -0700

    Initial Commit

---
+++ karma.conf.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+
+module.exports = function(config) {
+  config.set({
+    browsers: ['PhantomJS'],
+    frameworks: ['jasmine', 'jasmine-matchers', 'babel-polyfill'],
+    reporters: ['spec'],
+
+    // logLevel: config.LOG_DEBUG,
+
+    files: [
+      'test.webpack.js'
+    ],
+
+    preprocessors: {
+      'test.webpack.js': [ 'webpack', 'sourcemap' ]
+    },
+
+    webpack: {
+      devtool: '#inline-source-map',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            enforce: 'pre',
+            use: ['jshint-loader'],
+            exclude: /node_modules/
+          },
+          {
+            test: /\.js$/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['es2015']
+              }
+            },
+            exclude: /node_modules/
+          }
+        ]
+      },
+      resolve: {
+        alias: {
+          Src: './src'
+        }
+      }
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    }
+
+  })
+}
---
+++ src/AsyncTestHelper.js
@@ -0,0 +1,33 @@
+function startWith(fn) {
+  return waitFor(() => true, 0, fn)();
+}
+
+function waitFor(condition, conditonInterval = 100, fn = null) {
+  if (arguments.length == 2 && typeof time == 'function') {
+    fn = time; time = 100;
+  }
+
+  return function() {
+    return new Promise((resolve, reject) => {
+      var interval = window.setInterval(() => {
+        try {
+          if (condition()) {
+            window.clearInterval(interval);
+            resolve(fn && fn());
+          }
+        }
+        catch (e) {
+          reject(e);
+        }
+      }, conditonInterval);
+    });
+  };
+};
+
+function doThis(fn) {
+  return function() {
+    return waitFor(() => true, 1, fn);
+  };
+}
+
+export { startWith, waitFor, doThis };
