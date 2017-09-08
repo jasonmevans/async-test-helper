56cfd73680339643919bb8ba166e8c47a0d47ea8
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2017-09-08 13:51:17 -0700

    This isn't really necessary.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,2 @@
-var sources = require.context('../src', true, /\.js$/);
-sources.keys().forEach(sources);
-
 var tests = require.context('../test', true, /\.test\.js$/);
 tests.keys().forEach(tests);
