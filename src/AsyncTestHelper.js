f8d1fe31739e04c3fa46a9576f3d189ed8746118
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2017-09-09 18:01:01 -0700

    Added documentation

--- src/AsyncTestHelper.js
+++ src/AsyncTestHelper.js
@@ -1,18 +1,36 @@
+const trueFn = () => true;
+
 /**
- * [startWith description]
- * @param  {[type]} task [description]
- * @return {[type]}      [description]
+ * As the function name implies, this is where you start. This function
+ * returns the Promise wrapped by the `waitFor` function, and immediately
+ * resolves the Promise with the value returned by the passed in task
+ * function. While `waitFor` normally accepts a condition function for
+ * testing within the enclosed interval, the `startWith` function supplies
+ * the condition as `() => true` to immediately resolve the wrapped Promise.
+ * @param  {Function} task A function representing the starting task.
+ * @return {Promise}       A Promise of the value of the starting task.
  */
 function startWith(task) {
-  return waitFor(() => true, 0, task)();
+  return waitFor(trueFn, 0, task)();
 }
 
 /**
- * [waitFor description]
- * @param  {[type]} condition              [description]
- * @param  {Number} [conditonInterval=100] [description]
- * @param  {[type]} [task=null]            [description]
- * @return {[type]}                        [description]
+ * A function that checks a condition on an interval, and optionally runs a
+ * task afterward. Internally, the function returns a function returning a
+ * Promise of the condition being true, and the optional task's return value is
+ * passed into the Promise's resolve function allowing the value to be used
+ * later in the Promise chain. The semantics of the function make it passable
+ * into the `then` method of a Promise object.
+ * @param  {Function} condition            The test condition function that
+ *                                         returns a boolean
+ * @param  {Number} [conditonInterval=100] The number of milliseconds to wait
+ *                                         between testing the condition
+ * @param  {Function} [task=null]          An optional task function to execute
+ *                                         when the condition is true. The
+ *                                         return value of the task is passed to
+ *                                         the internal Promise's resolve.
+ * @return {Function}                      A function that returns a Promise of
+ *                                         the condition function being true.
  */
 function waitFor(condition, conditonInterval = 100, task = null) {
   if (arguments.length == 2 && typeof conditonInterval == 'function') {
@@ -20,7 +38,7 @@ function waitFor(condition, conditonInterval = 100, task = null) {
   }
   return function() {
     return new Promise((resolve, reject) => {
-      var interval = window.setInterval(() => {
+      const interval = window.setInterval(() => {
         try {
           if (condition()) {
             window.clearInterval(interval);
@@ -36,12 +54,18 @@ function waitFor(condition, conditonInterval = 100, task = null) {
 }
 
 /**
- * [doThis description]
- * @param  {[type]}  task [description]
- * @return {Boolean}      [description]
+ * A simple task function. The function calls `waitFor` internally, so it
+ * simply returns a function returning a Promise. Like `startWith`, this
+ * function supplies `() => true` as the condition causing the internal
+ * Promise to resolve immediately. The interval is supplied as `1` to make the
+ * resolution happen on the next tick after one millisecond. The semantics of
+ * the function make it passable into the `then` method of a Promise object.
+ * @param  {Function}  task The task function to run. Executes after 1ms
+ * @return {Function}       Returns a function returning a Promise that resolves
+ *                          after 1ms.
  */
 function doThis(task) {
-  return waitFor(() => true, 1, task);
+  return waitFor(trueFn, 1, task);
 }
 
 export { startWith, waitFor, doThis };
